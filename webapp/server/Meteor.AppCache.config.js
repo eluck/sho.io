307eb2d4649ba7f0618f6a68025057138f019656
eluck <mail@eluck.me>
2015-09-15 22:08:37 +0300

    Remove some needless files (reverted from commit 941d6de80606222902817f8c0e9ee9aea48ce9c9)

---
+++ webapp/server/Meteor.AppCache.config.js
@@ -0,0 +1,13 @@
+/*
+Meteor.startup(function () {
+  Meteor.AppCache.config({
+    onlineOnly: [
+      '/shower/',
+      '/.gitignore',
+      '/Gruntfile.js',
+      '/package.json',
+      '/node_modules'
+    ]
+  });
+});
+*/
